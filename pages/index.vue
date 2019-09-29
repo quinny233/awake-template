96cef4e969ebbceba2ce8cfbc0227b5a48d0e467
quinny233 <quinny233@live.co.uk>
2019-09-29 16:36:00 +0100

    content

--- pages/index.vue
+++ pages/index.vue
@@ -3,25 +3,85 @@
     <site-hero :title="title" :subtitle="subtitle" :image="featureImage">
     </site-hero>
     <main-section theme="one-column">
-     <div class="container">
-        <div class="columns">
-          <div class="column is-offset-2 is-8" >
-            <h2>About</h2>
-            <p>
-              <strong>
-                Edit this page.
-              </strong>
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-              eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-              sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-              ratione rem repudiandae sit!
-            </p>
+      <template v-slot:default>
+      
+      <div class="content">
+          <h3>Fake Heading</h3>
+          <p>
+            <strong>
+              Edit this page in<code>/pages/about.vue</code>to fit your needs.
+            </strong>
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
+            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
+            ratione rem repudiandae sit!
+          </p>
+
+          <h3>Another Fake Heading</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
+            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
+            ratione rem repudiandae sit!
+          </p>
+          <quote-card />
+          <h3>Still More Fake Content</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
+            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
+            ratione rem repudiandae sit! Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit.
+            <strong>Alias aut facere harum ipsam</strong> quibusdam quidem
+            quisquam saepe? Alias architecto blanditiis consequatur consequuntur
+            dicta ipsa iste modi provident, quod, similique veritatis.
+          </p>
+        </div>
+
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+              <p class="title">
+                Cool Stuff We Do
+              </p>
+              <p class="subtitle">
+                Sooo Cool
+              </p>
+              <figure class="image is-1by1">
+                <opti-image width="500" height="500" />
+              </figure>
+            </article>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+              <p class="title">
+                Other Cool Things
+              </p>
+              <p class="subtitle">
+                Just as cool
+              </p>
+              <figure class="image is-1by1">
+                <opti-image width="500" height="500" />
+              </figure>
+            </article>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+              <p class="title">
+                All the cool
+              </p>
+              <p class="subtitle">
+                that is all
+              </p>
+              <figure class="image is-1by1">
+                <opti-image width="500" height="500" />
+              </figure>
+            </article>
           </div>
         </div>
-      </div>
-      <template v-slot:default>
+        
         <!-- All Posts -->
         <posts-grid />
       </template>
