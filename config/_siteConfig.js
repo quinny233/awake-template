20e1069fe219015ec8ba9b09590160e62cb5f463
quinny233 <quinny233@live.co.uk>
2019-10-13 22:58:57 +0100

    revert

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -16,7 +16,7 @@ export default {
   logo: 'logo.png', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
-    id: 12345
+    id: process.env.GOOGLE_ANALYTICS_ID
   },
   mainMenu: [
     {
