d9a9b1fc6a44ba31953b191b9a52f391e7f62736
quinny233 <quinny233@live.co.uk>
2020-04-05 17:55:30 +0100

    Update writing.vue

--- pages/writing.vue
+++ pages/writing.vue
@@ -16,6 +16,10 @@
           <a href="/shopify-liquid-inspector"><p>www.quinny.dev/shopify-liquid-inspector</p></a>
           <small><time datetime="2020-02-03">3rd Feb 2020</time></small>
           <hr>
+          <h3>Preparing your eCommerce store for Black Friday</h3>
+          <a href="https://medium.com/@StrawbAgency/preparing-your-ecommerce-store-for-black-friday-28ba0103fe73"><p>https://medium.com/@StrawbAgency/preparing-your-ecommerce-store-for-black-friday-28ba0103fe73</p></a>
+          <small><time datetime="2018-11-13">13th November 2018</time></small>
+          <hr>
         </div>
       </template>
 
