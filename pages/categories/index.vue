b7c6d7e50591295551326f57710cfe009e62cc59
quinny233 <quinny233@live.co.uk>
2020-04-05 18:06:24 +0100

    testing

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,9 +1,27 @@
 <template>
   <div id="categories-page" class="page-wrapper categories-page">
     <site-hero title="Categories" image="https://picsum.photos/1800/1801" />
-    <main-section theme="one-column">
+    <main-section theme="sidebar-right">
       <categories-grid />
     </main-section>
+    <template v-slot:sidebar>
+        <h3 class="subtitle">
+          All Categories
+        </h3>
+        <div class="panel">
+          <nuxt-link
+            v-for="cat in allCats"
+            :key="cat.slug"
+            :to="`/categories/${cat.slug}`"
+            :class="{
+              'panel-block': true,
+              'is-active': cat.slug === $route.params.single
+            }"
+          >
+            {{ cat.name }}
+          </nuxt-link>
+        </div>
+      </template>
   </div>
 </template>
 <script>
