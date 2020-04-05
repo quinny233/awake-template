4cf585455fb55a0f040066b01726029a6ddefe37
quinny233 <quinny233@live.co.uk>
2020-04-05 18:14:55 +0100

    Update index.vue

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -33,9 +33,6 @@ export default {
       allCats: []
     }
   },
-  fetch({ store, params }) {
-    setPageData(store, { resource: 'category', slug: params.single })
-  },
   async created() {
     this.allCats = await this.$cms.category.getAll()
   }
