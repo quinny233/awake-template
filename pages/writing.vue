add2fc524db8cec19631b2d2ec8cd47f0af00321
quinny233 <quinny233@live.co.uk>
2020-04-05 17:25:07 +0100

    Update writing.vue

--- pages/writing.vue
+++ pages/writing.vue
@@ -1,34 +1,29 @@
 <template>
-  <div id="about-page" class="page-wrapper about-page content-page">
+  <div id="writing-page" class="page-wrapper content-page">
     <site-hero
-      title="Writing Directory"
-      subtitle="This is just a handy Directory of all the posts i've written in one place. Enjoy!"
+      title="Liam Quinn. Writing Directory"
+      subtitle="Just a handy Directory of all the posts I've written in one place. Enjoy!"
       image="/uploads/about-hero.jpg"
     ></site-hero>
     <main-section>
       <template v-slot:default>
         <div class="content">
           <h3>Name of Article</h3>
-          <a>www.quinny.dev/handleofartice</a>
-          <time datetime="2017-02-14">2017-02-14</time>
+          <a><p>www.quinny.dev/handleofartice</p></a>
+          <small><time datetime="2017-02-14">2017-02-14</time></small>
           <hr>
           <h3>Name of Article</h3>
-          <a>www.quinny.dev/handleofartice</a>
-          <time datetime="2017-02-14">2017-02-14</time> 
+          <a><p>www.quinny.dev/handleofartice</p></a>
+          <small><time datetime="2017-02-14">2017-02-14</time></small>
+          <hr> 
         </div>
       </template>
 
     </main-section>
   </div>
 </template>
-<script>
-import QuoteCard from '~/components/cards/QuoteCard'
-export default {
-  head() {
-    return {
-      title: `About | ${this.$siteConfig.siteName}`
-    }
-  },
-  components: { QuoteCard }
+<style>
+h3 {
+  margin: 0;
 }
-</script>
+</style>
